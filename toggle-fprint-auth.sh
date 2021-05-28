ed8d88e5d8c64667a2f88c5e0eb4f6c8c0cfa478
Malcolm Lalkaka <mlalkaka@gmail.com>
2021-05-28 09:45:44 -0700

    Log the success/failure of the writes to /sys

--- toggle-fprint-auth.sh
+++ toggle-fprint-auth.sh
@@ -7,12 +7,22 @@ grep -q close /proc/acpi/button/lid/LID/state
 if [ $? = 0 ]; then
     logger --tag lidfinger Disabling fingerprint sensor
     echo 0 > /sys/bus/usb/devices/1-9/authorized
+    if [ $? = 0 ]; then
+        logger --tag lidfinger Successfully disabled fingerprint sensor
+    else
+        logger --tag lidfinger Failed to disable fingerprint sensor
+    fi
 fi
 
 grep -q open /proc/acpi/button/lid/LID/state
 if [ $? = 0 ]; then
     logger --tag lidfinger Enabling fingerprint sensor
     echo 1 > /sys/bus/usb/devices/1-9/authorized
+    if [ $? = 0 ]; then
+        logger --tag lidfinger Successfully enabled fingerprint sensor
+    else
+        logger --tag lidfinger Failed to enable fingerprint sensor
+    fi
 fi
 
 logger --tag lidfinger Finished toggling fingerprint sensor
